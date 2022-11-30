dbaea88a6687b4253f6d1d49bc1a13f1e5ba9a70
Mohammed Sajjad <93313512+Mohsajjad@users.noreply.github.com>
2022-11-30 09:30:23 +0000

    Added custom functions for csv operations

---
+++ main.py
@@ -0,0 +1,62 @@
+#Creating headers for a file
+def parse_headers(header_line):
+    return header_line.strip().split(',')
+
+#Converting all values in the files to float and also handle missing values
+def parse_values(data_line):
+    values = []
+    for item in data_line.strip().split(','):
+        if item == '':
+            values.append(0.0)
+        else:
+            try:
+                values.append(float(item))
+            except ValueError:
+                values.append(item)
+    return values
+
+
+#Convert all the rows of a file into a dictionary 
+def create_item_dict(values, headers):
+    result = {}
+    for value, header in zip(values, headers):
+        result[header] = value
+    return result
+
+#Using the above funcitons to create a generic function to read a csv file
+def read_csv(path):
+    result = []
+    # Open the file in read mode
+    with open(path, 'r') as f:
+        # Get a list of lines
+        lines = f.readlines()
+        # Parse the header
+        headers = parse_headers(lines[0])
+        # Loop over the remaining lines
+        for data_line in lines[1:]:
+            # Parse the values
+            values = parse_values(data_line)
+            # Create a dictionary using values & headers
+            item_dict = create_item_dict(values, headers)
+            # Add the dictionary to the result
+            result.append(item_dict)
+    return result
+
+#writing a csv file
+def write_csv(items, path):
+    # Open the file in write mode
+    with open(path, 'w') as f:
+        # Return if there's nothing to write
+        if len(items) == 0:
+            return
+        
+        # Write the headers in the first line
+        headers = list(items[0].keys())
+        f.write(','.join(headers) + '\n')
+        
+        # Write one item per line
+        for item in items:
+            values = []
+            for header in headers:
+                values.append(str(item.get(header, "")))
+            f.write(','.join(values) + "\n")
